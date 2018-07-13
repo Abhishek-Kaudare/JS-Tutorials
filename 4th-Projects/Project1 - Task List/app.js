75349b13fb84028c367eef3dc2cef46b75b8e3fa
Abhishek Kaudare <2016.abhishek.kaudare@ves.ac.in>
2018-07-13 20:09:40 +0530

    q

--- 4th-Projects/Project1 - Task List/app.js
+++ 4th-Projects/Project1 - Task List/app.js
@@ -5,10 +5,10 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
-// Load all event listeners
+// Load all event listners
 loadEventListeners();
 
-// Load all event listeners
+// Load all event listners
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
@@ -18,28 +18,26 @@ function loadEventListeners() {
 function addTask(e) {
   if(taskInput.value === '') {
     alert('Add a task');
+  }else{
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append link to li
+    li.appendChild(link);
+    // Append li to ul
+    taskList.appendChild(li);
+    // Clear input
+    taskInput.value = '';
   }
-
-  // Create li element
-  const li = document.createElement('li');
-  // Add class
-  li.className = 'collection-item';
-  // Create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
-  // Create new link element
-  const link = document.createElement('a');
-  // Add class
-  link.className = 'delete-item secondary-content';
-  // Add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  // Append the link to li
-  li.appendChild(link);
-
-  // Append li to ul
-  taskList.appendChild(li);
-
-  // Clear input
-  taskInput.value = '';
-
   e.preventDefault();
+
 }
