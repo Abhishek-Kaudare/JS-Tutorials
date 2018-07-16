500efd370e92f6c88e6288508b3e1fd7c7027be7
Abhishek Kaudare <2016.abhishek.kaudare@ves.ac.in>
2018-07-16 22:23:52 +0530

    12

--- 4th-Projects/Project1 - Task List/app.js
+++ 4th-Projects/Project1 - Task List/app.js
@@ -4,15 +4,55 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+const delElement = document.querySelectorAll('.fa fa-remove');
 
 // Load all event listners
 loadEventListeners();
 
 // Load all event listners
 function loadEventListeners() {
+  // DOM Load Event
+  document.addEventListener('DOMContentLoaded',getTasks);
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks
+  filter.addEventListener('keyup',filterTasks);
+
 }
+
+// Get Tasks from LS
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task){
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(task));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append link to li
+    li.appendChild(link);
+    // Append li to ul
+    taskList.appendChild(li);
+  });
+  
+} 
 
 // Add Task
 function addTask(e) {
@@ -35,9 +75,86 @@ function addTask(e) {
     li.appendChild(link);
     // Append li to ul
     taskList.appendChild(li);
+    // Store in LS
+    storeTaskInLocalStorage(taskInput.value);
     // Clear input
     taskInput.value = '';
   }
   e.preventDefault();
+}
 
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) { 
+    // as the delete element is child element of anchor consisting the task so calling 
+    if(confirm('Are You Sure?')){
+      e.target.parentElement.parentElement.remove();
+    }
+    // reamove from LS
+    removeTaskFromLocalStorage(e.target.parentElement.parentElement.textContent)
+  }
+  e.preventDefault();
+}
+
+// remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task, index){
+    if (taskItem === task) {
+      console.log(task);
+      tasks.splice(index, 1);
+    }
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+// Clear tasks
+function clearTasks(e) {
+
+  if (confirm('Are You Sure?')){
+    while(taskList.firstChild){
+      taskList.removeChild(taskList.firstChild);
+    }
+  }
+
+  clearTasksFromLoacalStorage();
+  e.preventDefault();  
+}
+
+// Clear tasks form LS
+function clearTasksFromLoacalStorage() {
+  localStorage.clear();
+  
+}
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  taskList.querySelectorAll('.collection-item').forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    }else{
+      task.style.display = 'none';
+    }
+  });
 }
---
+++ 4th-Projects/Project2 - Loan Calculator/app.js
@@ -0,0 +1,81 @@
+// Listen for Submit
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+  // Hidden Results
+  document.getElementById('results').style.dispalay = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.dispalay = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
+
+
+// Calculate Results
+function calculateResults() {
+  
+  // UI Vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+  // Claculated Vars
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatePayments = parseFloat(years.value) * 12;
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatePayments);
+  const monthly = (principal*x*calculatedInterest)/(x-1);
+
+  if(isFinite(monthly)){
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatePayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatePayments)-principal).toFixed(2);
+    
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+  } else {
+    showError('Please check your input');
+  }
+}
+
+function showError(error,e) {
+  // Create a div
+  const errorDiv = document.createElement('div');
+  const closeError = document.createElement('button');
+  const closeSymbol = document.createElement('span');
+
+  closeError.setAttribute('type', 'button');
+  closeError.className = 'close';
+  closeError.setAttribute('data-dismiss' , 'alert');
+
+  closeSymbol.textContent = "×";
+  closeError.appendChild(closeSymbol);
+  // Get Elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Add Class
+  errorDiv.className = 'alert alert-danger alert-dismissible fade show';
+  // Create text node and append to div
+  errorDiv.appendChild(closeError);
+  errorDiv.appendChild(document.createTextNode(error));
+  // Insert error above Heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError,4000);
+  // e.preventDefault;
+}
+
+// Clear error
+function clearError() {
+  document.querySelector('.alert').remove();
+}
